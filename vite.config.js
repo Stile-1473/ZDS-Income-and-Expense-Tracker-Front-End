b825afb309683afc38a06109877f4323248c6397
Ghost1473 <stilesmvura@gmail.com>
2025-12-10 21:07:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     proxy: {
       '/api': {
        // target: 'https://zds-eldz.onrender.com',
-     target: 'http://zds.zw.com:8080/', //changing when pushing to git hub
+     target: 'https://billapi.duckdns.org/', //changing when pushing to git hub
 
         changeOrigin: true,
         secure: true
